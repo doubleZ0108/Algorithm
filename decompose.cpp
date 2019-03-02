b6a9243b5e221c08c5a2f4ffff32e03db35170ad
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-03-02 11:32:30 +0800

    将整数分解为质因数的乘积

--- decompose.cpp
+++ decompose.cpp
@@ -3,12 +3,14 @@
 /*将整数index分解为质因数的乘积*/
 void decompose(vector<int> &seq, int index)
 {
+	int n = index;	//要提前储存index,否则循环条件会一点点缩小
+
 	for (int i = 2; i < sqrt(index) + 1; ++i)
 	{
-		while (index%i == 0)
+		while (n%i == 0)
 		{
 			seq.push_back(i);
-			index /= i;
+			n /= i;
 		}
 	}
 }
