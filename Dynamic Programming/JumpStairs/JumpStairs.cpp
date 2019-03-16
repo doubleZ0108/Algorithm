964d99a73ece95170d41dd365206cc6b3cbe5026
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-03-16 10:32:27 +0800

    爬楼梯问题(递归/迭代/备忘录 三种算法) [x]double类型和long long类型结果不一致

---
+++ Dynamic Programming/JumpStairs/JumpStairs.cpp
@@ -0,0 +1,111 @@
+/*
+	Dynamic Programming --- 跳台阶问题
+
+Q:"一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级台阶。求该青蛙跳上 1 个 n
+	级的台阶总共有多少种跳法"。
+*/
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+/*递归算法求解跳台阶问题*/
+double JumpStairs_brutal(int n)
+{
+	if (n == 0) { return 0; }
+	else if (n == 1) { return 1; }
+	else if (n == 2) { return 2; }
+	else 
+		{ return JumpStairs_brutal(n - 1) + JumpStairs_brutal(n - 2); }
+}
+
+/*在递归算法中增加备忘录*/
+double JumpStairs_map(int n)
+{
+	static map<int, double> mymap;	//用静态图储存已经计算出来的值
+
+	if (n == 0) { return 0; }
+	else if (n == 1) { return 1; }
+	else if (n == 2) { return 2; }
+
+	if (mymap.count(n))	//在递归过程中, 如果静态图中已经存储过该值直接返回
+	{
+		return mymap[n];
+	}
+	else
+	{
+		double value= JumpStairs_map(n - 1) + JumpStairs_map(n - 2);
+		mymap[n] = value;	//将新值记录进静态图
+		return value;
+	}
+}
+
+/*迭代算法求解跳台阶问题*/
+double JumpStairs_loop(int n)
+{
+	if (n == 0) { return 0; }
+	else if (n == 1) { return 1; }
+	else if (n == 2) { return 2; }
+
+	double Fn, Fn_1 = 1, Fn_2 = 2;
+	for (int i = 3; i <= n; ++i)
+	{
+		Fn = Fn_1 + Fn_2;
+		Fn_1 = Fn_2;
+		Fn_2 = Fn;
+	}
+	return Fn;
+}
+
+int main(void)
+{
+	int n;
+	cout << "请输入楼梯总数: ";
+	while (cin >> n)
+	{
+		if (n >= 0) { break; }
+		else { cout << "楼梯总数应非负, 请重新输入: "; }
+	}
+	
+	int OpCode;
+	while (true)
+	{
+		cout << endl << "请输入你想选择的算法(1普通递归算法, 2备忘录递归算法, 3迭代算法, 0结束程序): ";
+		cin >> OpCode;
+		if (!OpCode) { break; }
+
+		switch (OpCode)
+		{
+		case 1: {
+			cout << "JumpStairs_brutal算法求得跳上" << n << "级台阶共有"
+				<< setiosflags(ios::fixed) << setprecision(0)		//设置以0位小数方式输出
+				<< JumpStairs_brutal(n) << "种跳法" << endl;
+
+			break;
+		}
+		case 2: {
+			cout << "JumpStairs_map算法求得跳上" << n << "级台阶共有"
+				<< setiosflags(ios::fixed) << setprecision(0)
+				<< JumpStairs_map(n) << "种跳法" << endl;
+
+			break;
+		}
+		case 3: {
+			cout << "JumpStairs_loop算法求得跳上" << n << "级台阶共有"
+				<< setiosflags(ios::fixed) << setprecision(0)
+				<< JumpStairs_loop(n) << "种跳法" << endl;
+
+			break;
+		}
+		default:
+			cout << "输入有误, 请重新输入(1普通递归算法, 2备忘录递归算法, 3迭代算法): ";
+			break;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
