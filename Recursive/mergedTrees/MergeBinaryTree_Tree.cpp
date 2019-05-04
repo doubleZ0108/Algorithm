6c9282ad6509bfc5337b8c2660a634530d3318dc
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-05-04 17:41:59 +0800

    合并二叉树 (用树存储) (三种方法思想相同,实现略有不同)

--- Recursive/mergedTrees/MergeBinaryTree_Tree.cpp
+++ Recursive/mergedTrees/MergeBinaryTree_Tree.cpp
@@ -1,3 +1,7 @@
+/*
+接口程序 & 测试程序: https://leetcode-cn.com/problems/merge-two-binary-trees/
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -38,4 +42,38 @@ TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
 	t1->left = mergeTrees(t1->left, t2->left);
 	t1->right = mergeTrees(t1->right, t2->right);
 	return t1;
+}
+
+
+/*完全重新开辟一棵新树*/
+TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+	TreeNode *root;
+	TreeNode *left1 = NULL, *left2 = NULL, *right1 = NULL, *right2 = NULL;
+
+	if (!t1 && !t2) { return NULL; }
+	else if (t1 && t2)
+	{
+		root = new TreeNode(t1->val + t2->val);
+		left1 = t1->left;
+		left2 = t2->left;
+		right1 = t1->right;
+		right2 = t2->right;
+	}
+	else if (t1 && !t2)
+	{
+		root = new TreeNode(t1->val);
+		left1 = t1->left;
+		right1 = t1->right;
+	}
+	else
+	{
+		root = new TreeNode(t2->val);
+		left2 = t2->left;
+		right2 = t2->right;
+	}
+
+	root->left = mergeTrees(left1, left2);
+	root->right = mergeTrees(right1, right2);
+
+	return root;
 }
