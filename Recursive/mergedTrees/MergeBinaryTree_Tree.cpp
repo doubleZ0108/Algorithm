f06d5fe2d47e98172f9214101ff0c47b54e6a782
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-05-04 17:40:27 +0800

    合并二叉树 (用树存储) -[](没有接口函数)

--- Recursive/mergedTrees/MergeBinaryTree_Tree.cpp
+++ Recursive/mergedTrees/MergeBinaryTree_Tree.cpp
@@ -9,9 +9,10 @@
  */
 class Solution {
 public:
+    /*以t1树储存结果, 不用另开辟一棵树*/
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
         if (!t1 && !t2) {
-		    return NULL;
+	    return NULL;
         }
         else if (t1 && !t2) {
             return t1;
@@ -27,3 +28,14 @@ public:
         }
     }
 };
+
+/*只分两种情况讨论, 当一方为NULL时, 直接返回另一方*/
+TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+	if (!t1) { return t2; }
+	if (!t2) { return t1; }
+
+	t1->val += t2->val;
+	t1->left = mergeTrees(t1->left, t2->left);
+	t1->right = mergeTrees(t1->right, t2->right);
+	return t1;
+}
